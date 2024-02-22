5c06f9e9f0e274b2a88e805a6a5fca3cf42f2532
dbaris <deanna.baris@gmail.com>
2024-02-21 22:25:25 -0500

    D3 added, test map works

---
+++ StitchIt/public/javascripts/test_map.js
@@ -0,0 +1,63 @@
+// import * as d3 from "https://cdn.skypack.dev/d3@7";
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import * as topojson from "https://cdn.skypack.dev/topojson@3.0.2"; 
+
+const outline = ({type: "Sphere"});
+const graticule = d3.geoGraticule10();
+var width = 900;
+const projectionSelect = d3.geoMercator();
+var height = fitWidth(projectionSelect);
+
+// proprotion to screen
+while (height > window.screen.height * .75){
+    console.log('resize...')
+    width = width * .9;
+    height = fitWidth(projectionSelect);
+}
+
+fetch('./data/world.json')
+    .then((response) => response.json()
+    )
+    .then((json) => {
+      // console.log(json)
+      const world = json;
+      const land = topojson.feature(world, world.objects.land);
+      const canvas = document.getElementById('test_map');
+      
+      const context = canvas.getContext("2d");
+      context.canvas.width  = width;
+      context.canvas.height = height;
+      context.fillStyle = "#fff";
+      context.fillRect(0, 0, width, height);
+      context.save();
+
+      function render(color) {
+          const path = d3.geoPath(projectionSelect, context);
+          context.fillStyle = context.strokeStyle = color;
+          context.save();
+          context.beginPath(), path(outline), context.clip();
+          context.beginPath(), path(graticule), context.globalAlpha = 0.3, context.stroke();
+          context.beginPath(), path(land), context.globalAlpha = 1.0, context.fill();
+          context.restore();
+          context.beginPath(), path(outline), context.stroke();
+      }
+
+    // context.translate(0, (height - heightRed) / 2);
+    // context.translate(0, height/2);
+    render("red");
+    context.restore();
+
+    context.save();
+    context.globalCompositeOperation = "multiply";
+    // context.translate(0, (height - heightBlue) / 2);
+    // render(blue, color2);
+    context.restore();
+    });
+
+function fitWidth(projection) {
+    const [[x0, y0], [x1, y1]] = d3.geoPath(projection.fitWidth(width, outline)).bounds(outline);
+    const dy = Math.ceil(y1 - y0), l = Math.min(Math.ceil(x1 - x0), dy);
+    projection.scale(projection.scale() * (l - 1) / l).precision(0.2);
+    console.log(dy);
+    return dy;
+}
